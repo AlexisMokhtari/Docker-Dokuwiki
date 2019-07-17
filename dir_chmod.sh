58848f6b593227277e2ec8f4732d843b5642b944
AlexisMokhtari <dev.mokhtaria@gmail.com>
2019-07-17 21:54:57 +0200

    Replace Www to www

--- dir_chmod.sh
+++ dir_chmod.sh
@@ -6,5 +6,5 @@ chmod -R g+w /var/www/html/lib/plugins
 chmod -R g+w /var/www/html/lib/tpl
 chgrp -R www-data /var/www/html/data
 chgrp -R www-data /var/www/html/conf
-chgrp -R www-data /var/Www/html/lib/plugins
+chgrp -R www-data /var/www/html/lib/plugins
 chgrp -R www-data /var/www/html/lib/tpl
